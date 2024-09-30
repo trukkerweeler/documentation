bc6089c88713153138a32441966d22fa4d224a2e
Tim Kent <ken22008@byui.edu>
2024-09-30 08:23:13 -0600

    updated nav, added std home pg

--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,8 @@
 import { loadHeaderFooter } from './utils.mjs';
-loadHeaderFooter();
+// loadHeaderFooter();
 
-const url = 'http://localhost:3000/sysdocs';
-
-// get the current year
-const year = new Date().getFullYear();
-// document.querySelector('#year').textContent = year;
-
-
-function getRecords () {
-    const main = document.querySelector('main');
-    
-    fetch(url, { method: 'GET' })
-
-    .then(response => response.json())
-    .then(records => {
-        // console.log(records);
-        const table = document.createElement('table');
-            const thead = document.createElement('thead');
-            const tbody = document.createElement('tbody');
-            const header = document.createElement('tr');
-            const td = document.createElement('td');
-            // const fieldList = ['DOCUMENT_ID', 'NAME', 'TYPE', 'STATUS', 'REVISION_LEVEL', 'ISSUE_DATE', 'SUBJECT', 'CREATE_BY', 'CREATE_DATE'];
-            for (let key in records[0]) {
-                // if (fieldList.includes(key)){
-                const th = document.createElement('th');
-                th.textContent = key;
-                header.appendChild(th);
-                // }
-            }
-            thead.appendChild(header);
-
-            for (let record of records) {
-                const tr = document.createElement('tr');
-                for (let key in record) {
-                    const td = document.createElement('td');
-                    let lastFour = key.slice(-4);
-                    switch (lastFour) {
-                        case 'DATE':
-                            td.textContent = record[key].slice(0,10);
-                            break;
-                        default:
-                            td.textContent = record[key];
-                    // tr.appendChild(td);
-                }
-                tr.appendChild(td);
-            }
-                tbody.appendChild(tr);
-            }
-
-            table.appendChild(thead);
-            table.appendChild(tbody);
-            main.appendChild(table);
-    })
-}
-
-getRecords();
+// append the button ncm with an anchor to ncms.html
+const ncmButton = document.querySelector('#documents');
+ncmButton.addEventListener('click', () => {
+  window.location.href = 'documents.html';
+});
